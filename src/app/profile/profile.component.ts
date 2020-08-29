87d5d24bcf8c2e6f22c6792a36c45d3ff4b40304
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-29 20:42:50 +0300

    imported profile service

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,21 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.css']
+// })
+// export class ProfileComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +24,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profiles: any;
+  repos: any;
+  username:string;
+  constructor(private profileService: ProfileService) {
+
+
+  }
+  findProfiles(){
+  	this.profileService.updateProfile(this.username);
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		this.profiles = profile;
+  	});
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
+  }
 
   ngOnInit(): void {
   }
