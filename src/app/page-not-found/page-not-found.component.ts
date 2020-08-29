312bf83341155eab9a7af256df882ed25260f935
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-29 21:28:12 +0300

    installed bootstrap

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,32 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-page-not-found',
+//   templateUrl: './page-not-found.component.html',
+//   styleUrls: ['./page-not-found.component.css']
+// })
+// export class PageNotFoundComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  template: `
+  <div class="alert alert-danger" role="alert">
+  <h1 class="text-center"><b>Sorry, we couldn't find what you're looking for</b></h1>
+  <hr>
+  <p class="text-center">Please check your URL!</p>
+</div>
+  `,
+  styles: []
 })
 export class PageNotFoundComponent implements OnInit {
 
