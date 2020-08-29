ca93d8db9ba5f92a8cea60f895d92aca0524f351
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-29 21:16:48 +0300

    added a HostListener in Directives

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+// import { Directive } from '@angular/core';
+
+// @Directive({
+//   selector: '[appHighlight]'
+// })
+// export class HighlightDirective {
+
+//   constructor() { }
+
+// }
+
+
+import { Directive ,ElementRef,HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
 
+  }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('grey');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
