833fdf4d22fc3c136eb2dec35c87fccb29910a60
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-07 13:48:17 +0300

    added repo img

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,23 +1,5 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-repo',
-//   templateUrl: './repo.component.html',
-//   styleUrls: ['./repo.component.css']
-// })
-// export class RepoComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
-
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
-
 
 @Component({
   selector: 'app-repo',
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,13 +1,3 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ProfileService {
-
-//   constructor() { }
-// }
-
 
 import { Injectable } from '@angular/core';
 
@@ -21,11 +11,11 @@ import {map} from 'rxjs/operators';
 export class ProfileService {
 
   private username: string;
-  private clientid = 'e5a8ffa24cd8e6788207'
-  private clientsecret ='29cc02966f0c1eeabf9cad139d113cfda8a1ed6d'
+  private clientid = ''
+  private clientsecret ='d33fb88510a2d371676f8766878560766a16b60e'
   constructor(private http:HttpClient) {
     console.log("service is now ready!");
-    this.username = 'fattym';
+    this.username = 'kiirudavid';
   }
   getProfileInfo(){
   	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" +    this.clientid +
